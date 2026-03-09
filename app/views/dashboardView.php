0b033347fe8462f907a6c76a4be84541e57e137f
olmelau <olmelau@gmail.com>
2026-03-09 12:04:03 +0100

    nuevo formulario en el dashboard

--- app/views/dashboardView.php
+++ app/views/dashboardView.php
@@ -13,10 +13,41 @@
         <button type="submit">SUMAR CACA</button>
     </form>
     <form action="index.php" method="post">
+        <!-- LLAMADA AL CONTROLLER -->
+        <input type="hidden" name="controller" value="grupos">
+        <input type="hidden" name="action" value="mostrarGrupos">
+        <button type="submit">MIS GRUPOS</button>
+    </form>
+    <form action="index.php" method="post">
+        <!-- LLAMADA AL CONTROLLER -->
+         <input type="hidden" name="controller" value="grupos">
+         <input type="hidden" name="action" value="nuevoGrupo">
+         <button type="submit">Nuevo Grupo</button>
+    </form>
+
+    <form action="index.php" method="post">
+        <input type="hidden" name="controller" value="grupos">
+        <input type="hidden" name="action" value="entrarAgrupo">
+        <button type="submit">Entrar a grupo</button>
+    </form>
+
+    <form action="index.php" method="post">
+        <input type="hidden" name="controller" value="perfilUser">
+        <input type="hidden" name="action" value="editarPerfil">
+        <button type="submit">Editar perfil</button>
+    </form>
+
+    <form action="index.php" method="post">
         <input type="hidden" name="controller" value="login">
         <input type="hidden" name="action" value="logout">
         <button type="submit">Cerrar Sesion</button>
     </form>
+
+
+
+    <div class="mostrarGrupos">
+        <p>Aqui se mostrará un scroll con los rankings de los grupos a los que pertenece el user</p>
+    </div>
 
 </body>
 </html>
