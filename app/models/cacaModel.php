86570a82d38dd7a4deb137df990959391b6b5570
AsierMartinez2000 <asiermm2000@gmail.com>
2026-04-14 19:40:42 +0200

    Añadimos cacas donde toca

--- app/models/cacaModel.php
+++ app/models/cacaModel.php
@@ -20,8 +20,30 @@ class CacaModel{
     
     $stmt = $this->db->prepare($sql);
     $stmt->bindParam(':id_usuario', $id_usuario, PDO::PARAM_INT);
+    $stmt->execute();
+
+    $sql2 = "UPDATE grupos_usuarios
+        SET cacas_user_grupo = cacas_user_grupo + 1 
+        WHERE id_usuario = :id_usuario";
+
+    $stmt = $this->db->prepare($sql2);
+    $stmt->bindParam(':id_usuario', $id_usuario, PDO::PARAM_INT);
+
+    $stmt->execute();
+
+    $sql3= "UPDATE grupos
+        SET total_cacas = 
+            (SELECT SUM(cacas_user_grupo) 
+            FROM grupos_usuarios
+            WHERE id_grupo = 
+                    (SELECT id_grupo
+                    FROM grupos_usuarios
+                    WHERE id_usuario = :id_usuario)
+            )";
+    $stmt = $this->db->prepare($sql3);
+    $stmt->bindParam(':id_usuario', $id_usuario, PDO::PARAM_INT);
+
     return $stmt->execute();
-    
     }
     
 
